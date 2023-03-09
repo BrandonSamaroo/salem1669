408a3f28378a6c576f80e01e2fadb4d77967f0cd
siobe34 <siobe14@hotmail.com>
2023-03-09 10:29:55 -0500

    basic theme with tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    theme: {
+        extend: {
+            colors: {
+                bg1: "rgb(var(--bg1) / <alpha-value>)",
+                bg2: "rgb(var(--bg2) / <alpha-value>)",
+                bg3: "rgb(var(--bg3) / <alpha-value>)",
+                onBg: "rgb(var(--onBg) / <alpha-value>)",
+                primary: "rgb(var(--primary) / <alpha-value>)",
+                onPrim: "rgb(var(--onPrim) / <alpha-value>)",
+            },
+        },
+    },
+};
