ee25b1e6a7bdeb7cf28f839f1edd278a0ecf1c3b
siobe34 <siobe14@hotmail.com>
2023-03-09 10:30:44 -0500

    prettier formatting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+    content: ["./src/**/*.{js,ts,jsx,tsx}"],
     darkMode: "class",
+    plugins: [],
     theme: {
         extend: {
             colors: {
