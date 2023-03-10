24d94adfdf7ce78d882d43b528ab55f4786dbd5e
siobe34 <siobe14@hotmail.com>
2023-03-10 02:11:07 -0500

    skeleton for game cards

--- src/pages/game/[game_id]/index.tsx
+++ src/pages/game/[game_id]/index.tsx
@@ -5,6 +5,8 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPersonWalkingArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 import { Button } from "@/components/Button";
+import { TryalCard } from "@/components/game/cards/TryalCard";
+import { AccusationCard } from "@/components/game/cards/AccusationCard";
 
 export default function HomePage() {
     const router = useRouter();
@@ -21,6 +23,16 @@ export default function HomePage() {
                     Leave Game
                     <FontAwesomeIcon icon={faPersonWalkingArrowRight} className='ml-2' />
                 </Button>
+                <div className='flex gap-4'>
+                    <TryalCard cardType='Constable' numberOfCards={1} />
+                    <TryalCard cardType='Witch' numberOfCards={1} />
+                    <TryalCard cardType='Not A Witch' numberOfCards={3} />
+                </div>
+                <div className='flex gap-4'>
+                    <AccusationCard cardType={1} numberOfCards={4} />
+                    <AccusationCard cardType={3} numberOfCards={2} />
+                    <AccusationCard cardType={7} numberOfCards={1} />
+                </div>
             </div>
         </>
     );
