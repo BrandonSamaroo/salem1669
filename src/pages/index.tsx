36b5a7ab26409e5563f516774cff8c8a3554175d
siobe34 <siobe14@hotmail.com>
2023-03-10 00:37:34 -0500

    cleaned HTML & separated buttons into components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import Head from "next/head";
 
+import { Button } from "@/components/Button";
+
 export default function HomePage() {
     return (
         <>
@@ -9,7 +11,7 @@ export default function HomePage() {
                 {/* // TODO add meta tags, favicons and web manifest */}
             </Head>
 
-            <div className='mx-4 mt-16 flex flex-col items-center gap-8'>
+            <div className='mx-4 mt-8 flex flex-col items-center gap-8'>
                 <span className='text-2xl'>Enter a name</span>
                 <input
                     className='w-4/5 rounded border border-primary/50 bg-inherit py-2 px-4 text-center text-onBg'
@@ -17,18 +19,13 @@ export default function HomePage() {
                     placeholder='Default Name'
                 />
                 <span className='text-2xl'>and</span>
-                {/* //TODO separate out into button component */}
-                <button className='rounded-lg border border-transparent bg-primary py-4 px-8 text-onPrim/90 text-onPrim hover:bg-primary/80 active:bg-primary'>
-                    Create New Game
-                </button>
+                <Button btnSize='large'>Create New Game</Button>
                 <span className='text-2xl'>...or</span>
-                <button className='rounded-lg border border-transparent bg-primary py-4 px-8 text-onPrim/90 text-onPrim hover:bg-primary/80 active:bg-primary'>
-                    Join Game
-                </button>
                 <div className='flex flex-col items-center'>
                     <label htmlFor='room-id'>Enter 6-digit Room ID</label>
                     <input className='w-4/5 rounded border border-primary/50 bg-inherit py-2 px-4 text-center text-onBg' type='text' id='room-id' />
                 </div>
+                <Button btnSize='large'>Join Game</Button>
             </div>
         </>
     );
