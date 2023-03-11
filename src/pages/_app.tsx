7e83cefdac2f5521b2fc8a19348c877ac4035b01
siobe34 <siobe14@hotmail.com>
2023-03-10 23:31:14 -0500

    dark mode by default

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,7 +9,8 @@ fontAwesomeConfig.autoAddCss = false;
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
-        <div className='app bg-bg1 text-onBg/80'>
+        // TODO change default dark mode to use media query of user-prefers-theme
+        <div className='app dark bg-bg1 text-onBg/80'>
             <header className='flex flex-wrap items-center justify-center gap-8 border-b-2 border-bg3 py-4'>Salem 1669</header>
             <main className='relative flex flex-col items-center justify-start scroll-smooth'>
                 <Component {...pageProps} />
