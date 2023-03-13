f4fd3c2800b34516558bbb7a6c894f873993a723
siobe34 <siobe14@hotmail.com>
2023-03-13 07:45:16 -0400

    type for Playing Cards, needs to be populated

---
+++ src/types/PlayingCardType.ts
@@ -0,0 +1,13 @@
+// TODO add all types of cards
+export type PlayingCardType =
+    | "Asylum"
+    | "Accusation"
+    | "Arson"
+    | "Matchmaker"
+    | "Robbery"
+    | "Piety"
+    | "Evidence"
+    | "Alibi"
+    | "Scapegoat"
+    | "Black Cat"
+    | "Stocks";
