0694635a13b18b3776cc7cbfdd12dbd0504dcf96
siobe <siobe14@hotmail.com>
2023-03-14 03:42:22 -0400

    setting aspect ratio instead of fixed height

--- src/components/game/cards/GameCard.tsx
+++ src/components/game/cards/GameCard.tsx
@@ -8,7 +8,10 @@ interface IGameCard {
 export const GameCard = ({ children, className, onClick }: IGameCard) => {
     return (
         <figure
-            className={`${className} relative flex h-[200px] w-[125px] flex-col items-center justify-center gap-8 rounded-md border-2 border-bg3 bg-bg1 hover:shadow-[0_0_30px_-20px_primary] hover:shadow-primary`}
+            // TODO need to make cards responsive
+            // REMOVEME
+            // className={`${className} relative flex h-[200px] w-[125px] flex-col items-center justify-center gap-8 rounded-md border-2 border-bg3 bg-bg1 hover:shadow-[0_0_30px_-20px_primary] hover:shadow-primary`}
+            className={`${className} relative flex aspect-[2/3] w-[125px] flex-col items-center justify-center gap-8 rounded-md border-2 border-bg3 bg-bg1 hover:shadow-[0_0_30px_-20px_primary] hover:shadow-primary`}
             onClick={onClick}
             role='button'
         >
