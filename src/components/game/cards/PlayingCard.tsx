7f890fa760f2ab39f805a6c72ee0fb786c88838a
siobe <siobe14@hotmail.com>
2023-03-14 03:09:05 -0400

    using generic GameCard component

--- src/components/game/cards/PlayingCard.tsx
+++ src/components/game/cards/PlayingCard.tsx
@@ -1,5 +1,7 @@
 import { PlayingCardType } from "@/types/PlayingCardType";
 
+import { GameCard } from "./GameCard";
+
 // TODO move to separate file
 interface IPlayingCard {
     className?: string;
@@ -10,13 +12,10 @@ interface IPlayingCard {
 
 export const PlayingCard = ({ className, onClick, cardType, numberOfCards }: IPlayingCard) => {
     return (
-        <div
-            className={`${className} relative flex h-[200px] w-[125px] flex-col items-center justify-center gap-8 border border-primary bg-bg3`}
-            onClick={onClick}
-        >
-            <h1 className='text-center text-xl'>Playing Card</h1>
+        <GameCard className={className} onClick={onClick}>
+            <h1 className='text-center text-xl'>{cardType}</h1>
             <span className='absolute top-0 left-0 m-2 flex h-6 w-6 items-center justify-center rounded-full border'>{numberOfCards}</span>
             <span className='rounded bg-primary px-2 py-1 text-onPrim'>{cardType}</span>
-        </div>
+        </GameCard>
     );
 };
--- src/components/game/cards/TryalCard.tsx
+++ src/components/game/cards/TryalCard.tsx
@@ -1,5 +1,7 @@
 import { TryalCardType } from "@/types/TryalCardType";
 
+import { GameCard } from "./GameCard";
+
 // TODO move to separate file
 interface ITryalCard {
     className?: string;
@@ -10,13 +12,10 @@ interface ITryalCard {
 
 export const TryalCard = ({ className, onClick, cardType, numberOfCards }: ITryalCard) => {
     return (
-        <div
-            className={`${className} relative flex h-[200px] w-[125px] flex-col items-center justify-center gap-8 border border-primary bg-bg3`}
-            onClick={onClick}
-        >
+        <GameCard className={className} onClick={onClick}>
             <h1 className='text-center text-xl'>Tryal Card</h1>
             <span className='absolute top-0 left-0 m-2 flex h-6 w-6 items-center justify-center rounded-full border'>{numberOfCards}</span>
             <span className='rounded bg-primary px-2 py-1 text-onPrim'>{cardType}</span>
-        </div>
+        </GameCard>
     );
 };
